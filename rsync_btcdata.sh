c668946b6883aaf76b03929c34171a10be2d9a55
SilentWatch <burgesschen1990@gmail.com>
2025-01-03 18:59:25 -0500

    sync

--- rsync_btcdata.sh
+++ rsync_btcdata.sh
@@ -19,7 +19,8 @@ sync_directories() {
     # -a: archive mode (preserves permissions etc)
     # -u: only copy newer files
     rsync -au "$from_dir/" "$to_dir/"
-
+    # Log the sync operation with timestamp
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] Synced $from_dir to $to_dir" >> "/var/log/btc_sync.log"
     echo "Directory sync completed from $from_dir to $to_dir"
 }
 
